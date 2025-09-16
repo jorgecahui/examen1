07c7c85a71a035e7286bd3f0be331cd1e92e5b64
jorgecahui <jorgecahui347@gmail.com>
2025-09-16 13:30:37 -0500

    a

---
+++ jlcc-config-server/src/main/java/com/example/jlccconfigserver/JlccConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jlccconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlccConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JlccConfigServerApplication.class, args);
+    }
+
+}
---
+++ jlcc-config-server/src/test/java/com/example/jlccconfigserver/JlccConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jlccconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlccConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
